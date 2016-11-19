2e62bec2069f1337966187c6d9a2462b95e64fb6
Dustin Hartzler <Dustin@HartzlerDM.com>
2016-11-19 09:42:49 -0500

    Updated Ninja Forms

--- wp-content/plugins/ninja-forms/includes/Fields/Product.php
+++ wp-content/plugins/ninja-forms/includes/Fields/Product.php
@@ -23,7 +23,7 @@ class NF_Fields_Product extends NF_Abstracts_Input
 
     protected $_settings = array( 'product_use_quantity', 'product_price', 'product_type', 'product_type' );
 
-    protected $_settings_exclude = array( 'input_limit_set' );
+    protected $_settings_exclude = array( 'input_limit_set', 'disable_input', 'required' );
 
     public function __construct()
     {
@@ -32,12 +32,11 @@ class NF_Fields_Product extends NF_Abstracts_Input
         $this->_nicename = __( 'Product', 'ninja-forms' );
 
         $this->_settings[ 'product_price' ][ 'width' ] = 'full';
-        $this->_settings[ 'required' ][ 'deps' ][ 'product_use_quantity' ] = 1;
 
         add_filter( 'ninja_forms_merge_tag_value_product', array( $this, 'merge_tag_value' ), 10, 2 );
 
         add_filter( 'ninja_forms_localize_field_' . $this->_name, array( $this, 'filter_required_setting' ) );
-        add_filter( 'ninja_forms_localize_field_' . $this->_name . '_preview', array( $this, 'filter_required_setting_preview' ) );
+        add_filter( 'ninja_forms_localize_field_' . $this->_name . '_preview', array( $this, 'filter_required_setting' ) );
     }
 
     public function process( $product, $data )
@@ -46,7 +45,8 @@ class NF_Fields_Product extends NF_Abstracts_Input
 
         foreach( $data[ 'fields' ] as $key => $field ){
 
-            if( ! in_array( $field[ 'type' ], $this->processing_fields ) ) continue;
+
+            if( ! isset ( $field[ 'type' ] ) || ! in_array( $field[ 'type' ], $this->processing_fields ) ) continue;
 
             $type = $field[ 'type' ];
 
@@ -98,14 +98,6 @@ class NF_Fields_Product extends NF_Abstracts_Input
 
     public function filter_required_setting( $field )
     {
-        if( 0 == $field->get_setting( 'product_use_quantity', 0 ) ) {
-            $field->update_setting('required', 0);
-        }
-        return $field;
-    }
-
-    public function filter_required_setting_preview( $field )
-    {
         if( ! isset( $field[ 'settings' ][ 'product_use_quantity' ] ) || 1 != $field[ 'settings' ][ 'product_use_quantity' ] ) {
             $field[ 'settings' ][ 'required' ] = 0;
         }
@@ -114,10 +106,10 @@ class NF_Fields_Product extends NF_Abstracts_Input
 
     public function merge_tag_value( $value, $field )
     {
-        $product_price = ( isset( $field[ 'product_price' ] ) ) ? str_replace( '$', '', $field[ 'product_price' ] ) : 0;
+        $product_price = preg_replace ('/[^\d,\.]/', '', $field[ 'product_price' ] );
+
         $product_quantity = ( isset( $field[ 'product_use_quantity' ] ) && 1 == $field[ 'product_use_quantity' ] ) ? $value : 1;
 
-        // TODO: Extract a higher level currency formatting based on settings.
-        return '$' . number_format( $product_price * $product_quantity, 2 );
+        return number_format( $product_price * $product_quantity, 2 );
     }
 }
