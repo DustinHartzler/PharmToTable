43cca65374d86fcbdddcebbaeaf947188ede611f
Dustin Hartzler <Dustin@HartzlerDM.com>
2017-08-29 18:33:10 -0400

    Added MailChimp Form

--- wp-content/themes/squared/popup-50.php
+++ wp-content/themes/squared/popup-50.php
@@ -13,38 +13,38 @@
 			  <p>Success! Now check your email to confirm your subscription.</p>
 			</div>
 			<!--  Form starts here  -->
-			<form id="ck_subscribe_form" class="ck_subscribe_form" action="https://app.convertkit.com/landing_pages/20589/subscribe" data-remote="true">
-			  <input type="hidden" value="{&quot;form_style&quot;:&quot;full&quot;,&quot;embed_style&quot;:&quot;modal&quot;,&quot;embed_trigger&quot;:&quot;scroll_percentage&quot;,&quot;scroll_percentage&quot;:&quot;70&quot;,&quot;delay_seconds&quot;:&quot;10&quot;,&quot;display_position&quot;:&quot;br&quot;,&quot;display_devices&quot;:&quot;all&quot;,&quot;days_no_show&quot;:&quot;15&quot;,&quot;converted_behavior&quot;:&quot;custom&quot;}" id="ck_form_options">
-			  <input type="hidden" name="id" value="20589" id="landing_page_id">
-			  <div class="ck_errorArea">
-				<div id="ck_error_msg" style="display:none">
-				  <p>There was an error submitting your subscription. Please try again.</p>
-				</div>
-			  </div>
-			  <div class="ck_control_group ck_first_name_field_group">
-				<input type="text" name="first_name" class="ck_first_name" id="ck_firstNameField" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your first name here'"  placeholder="Enter your first name here" />
-			  </div>
-			  <div class="ck_control_group ck_email_field_group">
-				<input type="text" name="email" class="ck_email_address" id="ck_emailField"  onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your best email here'" placeholder="Enter your best email here" required/>
-			  </div>
-			  <div class="ck_control_group ck_captcha2_h_field_group ck-captcha2-h" style="position: absolute !important;left: -999em !important;">
-				<label class="ck_label" for="ck_captcha2_h">We use this field to detect spam bots. If you fill this in, you will be marked as a spammer.</label>
-				<input type="text" name="captcha2_h" class="ck-captcha2-h" id="ck_captcha2_h">
-			  </div>
-
-				<label class="ck_checkbox" style="display:none;">
-				  <input class="optIn ck_course_opted" name="course_opted" type="checkbox" id="optIn" checked />
-				  <span class="ck_opt_in_prompt">I'd like to receive the free email course.</span>
-				</label>
-
-			  <button class="subscribe_button ck_subscribe_button button fields" id="ck_subscribe_button">
-				Send My eBook Now!
-			  </button>
-			  <span class="ck_guarantee">
-
-				  <a class="ck_powered_by" href="https://yourwebsiteengineer.com/convertkit">Powered by ConvertKit</a>
-			  </span>
-			</form>
+			<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//YourWebsiteEngineer.us2.list-manage.com/subscribe/post?u=52e28e4b77d42ca4c190e39ef&amp;id=b0b7b955f6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<h2>Subscribe to our mailing list</h2>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-NAME">Name </label>
+	<input type="text" value="" name="NAME" class="" id="mce-NAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_52e28e4b77d42ca4c190e39ef_b0b7b955f6" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='NAME';ftypes[1]='text';fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
 		</div>
 		<a class="close-reveal-modal"><img src="<?php bloginfo('template_url'); ?>/images/newsletter/ico-close.png" alt=""/></a>
 		<div class="cl"></div>
