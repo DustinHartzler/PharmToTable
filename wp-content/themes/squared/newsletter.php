a242332296c42f6cdad94539e31af3e030c49815
Dustin Hartzler <Dustin@HartzlerDM.com>
2017-08-29 18:14:47 -0400

    More CSS

--- wp-content/themes/squared/newsletter.php
+++ wp-content/themes/squared/newsletter.php
@@ -7,7 +7,7 @@ Template Name: Newsletter Page
     <div class="body-container">
         <div class="body wrapper-404 clearfix">
 
-            <div class="left">
+            <div class="left-side">
                 <img src="<?php bloginfo('template_url'); ?>/img/404/404.png" id="logo2" class="leadstyle-image" style="max-width: 268px;"><p class="leadstyle-text">Holy Smokes! Unfortunately, the information you're looking for is unavailable.</p>
                 <p class="style2">BUT WAIT!</p>
                 <p class="style3">Don't leave empty handed. <br />Grab this <a href="#" data-reveal-id="myModal">free ebook</a>!</p>
