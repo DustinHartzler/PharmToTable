cd78a98610531cb4b1bd334708de3a1b11c22582
Dustin Hartzler <Dustin.Hartzler@Automattic.com>
2021-01-04 09:42:21 -0500

    Removed Breadcrumbs from the FB Optin page

--- wp-content/themes/storefront-child-pharm/page-fb-optin.php
+++ wp-content/themes/storefront-child-pharm/page-fb-optin.php
@@ -103,24 +103,7 @@
     </header>
     <!-- End Header -->
 
- <!-- Start Breadcrumb
-============================================= -->
-<div class="breadcrumb-area shadow dark bg-fixed text-light" style="background-image: url(assets/img/2440x1578.png);">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h1><?php the_title(); ?></h1>
-            </div>
-            <div class="col-md-6 text-right">
-                <ul class="breadcrumb">
-                    <li><a href="<?php echo get_home_url(); ?>"><i class="fas fa-home"></i> Home</a></li>
-                    <li class="active"><?php the_title(); ?></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Breadcrumb -->
+
 
  <!-- Start Page Block
 ============================================= -->
