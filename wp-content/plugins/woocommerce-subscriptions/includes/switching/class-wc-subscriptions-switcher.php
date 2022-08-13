9127cd561ff0a658d821db8a26b51bd93fcca601
Dustin Hartzler <Dustin@HartzlerDM.com>
2022-08-13 16:43:01 -0400

    Updated WooCommerce Subscriptions

--- wp-content/plugins/woocommerce-subscriptions/includes/switching/class-wc-subscriptions-switcher.php
+++ wp-content/plugins/woocommerce-subscriptions/includes/switching/class-wc-subscriptions-switcher.php
@@ -47,21 +47,21 @@ class WC_Subscriptions_Switcher {
 		// We need to create subscriptions on checkout and want to do it after almost all other extensions have added their products/items/fees
 		add_action( 'woocommerce_checkout_order_processed', array( __CLASS__, 'process_checkout' ), 50, 2 );
 
-		// We need to create subscriptions on checkout and want to do it after almost all other extensions have added their products/items/fees
-		if ( class_exists( 'Automattic\WooCommerce\Blocks\Package' ) && ( version_compare( \Automattic\WooCommerce\Blocks\Package::get_version(), '6.3.0', '>=' ) || \Automattic\WooCommerce\Blocks\Package::is_experimental_build() ) ) {
-			add_action( 'woocommerce_blocks_checkout_order_processed', array( __CLASS__, 'process_checkout' ), 50, 1 );
-		} else {
-			add_action( '__experimental_woocommerce_blocks_checkout_order_processed', array( __CLASS__, 'process_checkout' ), 50, 1 );
-		}
+		// Same as above for WooCommerce Blocks.
+		add_action( 'woocommerce_store_api_checkout_order_processed', array( __CLASS__, 'process_checkout' ), 50, 1 );
 
 		// When creating an order, add meta if it's for switching a subscription
 		add_action( 'woocommerce_checkout_update_order_meta', array( __CLASS__, 'add_order_meta' ), 10, 2 );
 
-		// From blocks - When creating an order, add meta if it's for switching a subscription
-		if ( class_exists( 'Automattic\WooCommerce\Blocks\Package' ) && ( version_compare( \Automattic\WooCommerce\Blocks\Package::get_version(), '6.3.0', '>=' ) || \Automattic\WooCommerce\Blocks\Package::is_experimental_build() ) ) {
-			add_action( 'woocommerce_blocks_checkout_update_order_meta', array( __CLASS__, 'add_order_meta' ), 10, 1 );
-		} else {
-			add_action( '__experimental_woocommerce_blocks_checkout_update_order_meta', array( __CLASS__, 'add_order_meta' ), 10, 1 );
+		// Same as above for WooCommerce Blocks.
+		if ( class_exists( 'Automattic\WooCommerce\Blocks\Package' ) ) {
+			if ( version_compare( \Automattic\WooCommerce\Blocks\Package::get_version(), '7.2.0', '>=' ) ) {
+				add_action( 'woocommerce_store_api_checkout_update_order_meta', array( __CLASS__, 'add_order_meta' ), 10, 1 );
+			} elseif ( version_compare( \Automattic\WooCommerce\Blocks\Package::get_version(), '6.3.0', '>=' ) ) {
+				add_action( 'woocommerce_blocks_checkout_update_order_meta', array( __CLASS__, 'add_order_meta' ), 10, 1 );
+			} else {
+				add_action( '__experimental_woocommerce_blocks_checkout_update_order_meta', array( __CLASS__, 'add_order_meta' ), 10, 1 );
+			}
 		}
 
 		// Don't allow switching to the same product
@@ -551,9 +551,11 @@ class WC_Subscriptions_Switcher {
 			return;
 		}
 
-		$switch_url  = esc_url( self::get_switch_url( $item_id, $item, $subscription ) );
-		$switch_text = get_option( WC_Subscriptions_Admin::$option_prefix . '_switch_button_text', __( 'Upgrade or Downgrade', 'woocommerce-subscriptions' ) );
-		$switch_link = sprintf( '<a href="%s" class="wcs-switch-link button">%s</a>', $switch_url, $switch_text );
+		$switch_url     = esc_url( self::get_switch_url( $item_id, $item, $subscription ) );
+		$switch_text    = apply_filters( 'woocommerce_subscriptions_switch_link_text', get_option( WC_Subscriptions_Admin::$option_prefix . '_switch_button_text', __( 'Upgrade or Downgrade', 'woocommerce-subscriptions' ) ), $item_id, $item, $subscription );
+		$switch_classes = apply_filters( 'woocommerce_subscriptions_switch_link_classes', array( 'wcs-switch-link', 'button' ), $item_id, $item, $subscription );
+
+		$switch_link    = sprintf( '<a href="%s" class="%s">%s</a>', $switch_url, implode( ' ', (array) $switch_classes ), $switch_text );
 
 		echo wp_kses( apply_filters( 'woocommerce_subscriptions_switch_link', $switch_link, $item_id, $item, $subscription ), array( 'a' => array( 'href' => array(), 'title' => array(), 'class' => array() ) ) ); // phpcs:ignore WordPress.Arrays.ArrayDeclarationSpacing.AssociativeArrayFound
 	}
--- wp-content/plugins/woocommerce-subscriptions/vendor/composer/autoload_static.php
+++ wp-content/plugins/woocommerce-subscriptions/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit5866e8f1ce7992c025ec046bcf66d84f
+class ComposerStaticInit391e397d37e6c11617453a67b8c8fa47
 {
     public static $prefixLengthsPsr4 = array (
         'C' => 
@@ -129,9 +129,9 @@ class ComposerStaticInit5866e8f1ce7992c025ec046bcf66d84f
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit5866e8f1ce7992c025ec046bcf66d84f::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit5866e8f1ce7992c025ec046bcf66d84f::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit5866e8f1ce7992c025ec046bcf66d84f::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit391e397d37e6c11617453a67b8c8fa47::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit391e397d37e6c11617453a67b8c8fa47::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit391e397d37e6c11617453a67b8c8fa47::$classMap;
 
         }, null, ClassLoader::class);
     }
