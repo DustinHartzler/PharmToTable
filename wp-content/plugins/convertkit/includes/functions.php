127a53201092ee4b0636fa69566f008a7126d24b
Dustin Hartzler <Dustin@HartzlerDM.com>
2023-09-02 07:30:03 -0400

    Updated ConverKit

--- wp-content/plugins/convertkit/includes/functions.php
+++ wp-content/plugins/convertkit/includes/functions.php
@@ -306,6 +306,26 @@ function convertkit_get_sign_in_url() {
 }
 
 /**
+ * Helper method to return the URL the user needs to visit to manage thier billing.
+ *
+ * @since   2.2.7
+ *
+ * @return  string  ConvertKit Billing URL.
+ */
+function convertkit_get_billing_url() {
+
+	return add_query_arg(
+		array(
+			'utm_source'  => 'wordpress',
+			'utm_term'    => get_locale(),
+			'utm_content' => 'convertkit',
+		),
+		'https://app.convertkit.com/account_settings/billing/'
+	);
+
+}
+
+/**
  * Helper method to return the URL the user needs to visit on the ConvertKit app to obtain their API Key and Secret.
  *
  * @since   1.9.6.1
@@ -439,21 +459,20 @@ function convertkit_select2_enqueue_styles() {
  */
 function convertkit_get_file_contents( $local_file ) {
 
-	// Call globals.
-	global $wp_filesystem;
-
-	// Load filesystem class.
-	require_once ABSPATH . 'wp-admin/includes/file.php';
-
-	// Initiate.
-	WP_Filesystem();
-
 	// Bail if the file doesn't exist.
-	if ( ! $wp_filesystem->exists( $local_file ) ) {
+	if ( ! file_exists( $local_file ) ) {
+		return '';
+	}
+
+	// Read file.
+	$contents = file_get_contents( $local_file ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+
+	// Return an empty string if the contents of the file could not be read.
+	if ( ! $contents ) {
 		return '';
 	}
 
 	// Return file's contents.
-	return $wp_filesystem->get_contents( $local_file );
+	return $contents;
 
 }
--- wp-content/plugins/convertkit/readme.txt
+++ wp-content/plugins/convertkit/readme.txt
@@ -3,9 +3,9 @@ Contributors: nathanbarry, growdev, travisnorthcutt, ggwicz
 Donate link: https://convertkit.com
 Tags: email marketing, email newsletter, newsletter, subscribers, membership
 Requires at least: 5.0
-Tested up to: 6.2.2
+Tested up to: 6.3
 Requires PHP: 5.6.20
-Stable tag: 2.2.6
+Stable tag: 2.2.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -137,6 +137,13 @@ Full Plugin documentation can be found [here](https://help.convertkit.com/en/art
 7. Track subscriber growth
 
 == Changelog ==
+
+### 2.2.8 2023-08-07
+* Fix: Use `file_get_contents` instead of `WP_Filesystem` for reading plugin files, to avoid fatal error on activation when file ownership/permission issues occur
+* Fix: Block Editor: Prevent block error in WordPress 6.0 and lower by checking if `useAnchor` is available
+
+### 2.2.7 2023-07-24
+* Added: Contact Form 7: Option to Enable Creator Network Recommendations modal on individual Forms at Settings > ConvertKit > Contact Form 7
 
 ### 2.2.6 2023-07-18
 * Added: Form Trigger: Block: When no API Key specified, link to Setup Wizard in a popup window to complete setup
